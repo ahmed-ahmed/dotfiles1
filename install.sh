290700da0b6e7520a493d153f10f5af4aa535768
Ahmed M Ahmed-D25 <aahmed@cas.org>
2018-03-29 15:56:17 -0400

    installing powerline fonts

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 sudo apt-get update && sudo apt-get -y install zsh
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo apt-get install fonts-powerline
 
 # backup dotfiles
 mkdir ~/bk
-mv .zshrc ~/bk
+if [ -f ~/.zshrc ]; then
+   mv ~/.zshrc ~/bk
+fi
 
 # install dotfiles 
 ln -sv ./.zshrc ~
