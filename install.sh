1e613465d6edc34e79cb343f234cd1cb0f9dfb5d
Ahmed M Ahmed-D25 <aahmed@cas.org>
2018-03-29 15:06:41 -0400

    backup exsisting dotfiles

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@ sudo apt-get update && sudo apt-get -y install zsh
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# backup dotfiles
+mkdir ~/bk
+mv .zshrc ~/bk
 
-#ln -sv “~/.dotfiles/runcom/.bash_profile” ~
+# install dotfiles 
+ln -sv “./.zshrc” ~
 
