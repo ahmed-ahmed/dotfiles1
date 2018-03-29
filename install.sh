e0618802a9b475a8e95f48d3cca974560f3a0dab
Ahmed M Ahmed-D25 <aahmed@cas.org>
2018-03-29 14:29:25 -0400

    installing zsh and ohmyzsh

---
+++ install.sh
@@ -0,0 +1,8 @@
+# install zsh
+sudo apt-get update && sudo apt-get -y install zsh
+chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+#ln -sv “~/.dotfiles/runcom/.bash_profile” ~
+
